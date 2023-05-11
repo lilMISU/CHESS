1aa4888400ab2efa1e7ef2444259f2c5a379daac
lilMISU <119946273+lilMISU@users.noreply.github.com>
2023-05-11 19:26:40 +0300

    Add files via upload

---
+++ chess_client/CHESS/CHESS/King.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHESS
+{
+    class King : Piece
+    {
+        public override void  ColorChange(Cell[,] cell)
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                        cell[i, j].BackColor = Color.BlanchedAlmond;
+                    else
+                        cell[i, j].BackColor = Color.White;
+
+                }
+            }
+
+        }
+    }
+}
